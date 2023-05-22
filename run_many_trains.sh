2d9b6bad7a18ef89c634a3598245b6a39c23e1f5
Grzegorz Jurdziński <g.jurdzinski@gmail.com>
2023-05-22 21:28:38 +0200

    Add new configs and temp comment out some runs.

--- run_many_trains.sh
+++ run_many_trains.sh
@@ -8,4 +8,7 @@ python \
     model/reward/instructor/configs/multi/reward-model-reward-deberta-v3-base-newsroom-inf-multi-train.yml
 python \
     model/reward/instructor/trainer.py\
-    model/reward/instructor/configs/multi/reward-model-reward-deberta-v3-large-newsroom-inf-multi-train.yml
+    model/reward/instructor/configs/multi/reward-model-deberta-v3-base-newsroom-inf-multi-train.yml
+python \
+    model/reward/instructor/trainer.py\
+    model/reward/instructor/configs/multi/reward-model-deberta-v3-large-newsroom-inf-multi-train.yml
