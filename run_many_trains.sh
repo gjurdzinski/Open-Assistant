bba3497c4aca877eb57b0091f87a5348be5cc9a0
Grzegorz Jurdziński <g.jurdzinski@gmail.com>
2023-05-24 09:56:28 +0200

    Rename config files and add new ones.

--- run_many_trains.sh
+++ run_many_trains.sh
@@ -2,13 +2,7 @@
 
 python \
     model/reward/instructor/trainer.py\
-    model/reward/instructor/configs/multi/reward-model-deberta-v3-small-newsroom-inf-multi-train.yml
+    model/reward/instructor/configs/multi/deberta-base-newsroom-inf.yml
 python \
     model/reward/instructor/trainer.py\
-    model/reward/instructor/configs/multi/reward-model-reward-deberta-v3-base-newsroom-inf-multi-train.yml
-python \
-    model/reward/instructor/trainer.py\
-    model/reward/instructor/configs/multi/reward-model-deberta-v3-base-newsroom-inf-multi-train.yml
-python \
-    model/reward/instructor/trainer.py\
-    model/reward/instructor/configs/multi/reward-model-deberta-v3-large-newsroom-inf-multi-train.yml
+    model/reward/instructor/configs/multi/deberta-large-newsroom-inf.yml
