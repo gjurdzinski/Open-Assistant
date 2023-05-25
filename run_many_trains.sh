6062e9515c637b6450fd60aa1b7540872890963f
Grzegorz Jurdziński <g.jurdzinski@gmail.com>
2023-05-25 08:59:06 +0200

    Edit deberta base train config.

--- run_many_trains.sh
+++ run_many_trains.sh
@@ -3,3 +3,6 @@
 python \
     model/reward/instructor/trainer.py\
     model/reward/instructor/configs/multi/temp.yml
+python \
+    model/reward/instructor/trainer.py\
+    model/reward/instructor/configs/multi/deberta-base-newsroom-inf.yml
